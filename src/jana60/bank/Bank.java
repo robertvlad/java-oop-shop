e3e61c705ff7f4232d89c365a6f10f0692f94cc0
robertvlad <vladrob96@gmail.com>
2023-05-09 15:37:47 +0300

    Update

--- src/jana60/bank/Bank.java
+++ src/jana60/bank/Bank.java
@@ -1,5 +1,53 @@
 package jana60.bank;
 
-public class Bank {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Bank {
+	
+	public static void main(String[] args) {
+		
+		Random rnd = new Random();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Inserisci il tuo nome per creare il tuo nuovo conto bancario: ");
+		String nome = sc.nextLine();
+		
+		Conto contoUtente = new Conto(rnd.nextInt(1,1000),nome);
+
+		int sceltaUtente = -1;
+		
+		while(sceltaUtente != 3) {
+			
+			System.out.println("Cosa vuoi fare? \n 0-versare somma \n 1-prelevare somma \n -2 informazioni conto \n -3 uscire");
+			sceltaUtente = sc.nextInt();		
+			
+			if(sceltaUtente == 0) {
+				
+				System.out.println("quanto vuoi versare?: ");
+				float cifraVersare = sc.nextFloat();
+				
+				contoUtente.versare(cifraVersare);
+				
+				System.out.println(contoUtente.getSaldo());
+				
+			}else if(sceltaUtente == 1) {
+				
+				System.out.println("quanto vuoi prelevare?: ");
+				float cifraPrelevare = sc.nextFloat();
+				
+				contoUtente.prelevare(cifraPrelevare);
+				
+				System.out.println(contoUtente.getSaldo());
+				
+			}else if(sceltaUtente == 2) {
+				
+				System.out.println(contoUtente);
+				
+			}		
+		}
+		
+		sc.close();
+	}
 }
--- src/jana60/bank/Conto.java
+++ src/jana60/bank/Conto.java
@@ -1,5 +1,65 @@
 package jana60.bank;
 
 public class Conto {
-
+	
+	private int numeroConto;
+	private String nomeProprietario;
+	private float saldo;	
+	
+	public Conto(int numeroConto, String nomeProprietario) {
+		
+		this.saldo = 0;
+		this.numeroConto = numeroConto;
+		this.nomeProprietario = nomeProprietario;		
+	}
+	
+	
+	public int getNumeroConto() {
+		
+		return this.numeroConto;
+	}
+	
+	public float getSaldo() {
+		
+		return this.saldo;
+	}
+	
+	public String getNomeProprietario() {
+		
+		return this.nomeProprietario;
+	}
+	
+	public void setNomeProprietario(String nomeProprietario) {
+		
+		this.nomeProprietario = nomeProprietario;
+	}
+	
+	
+	public void versare(float sommaDaVersare) {
+		
+		this.saldo += sommaDaVersare;		
+	}
+	
+	public void prelevare(float sommaDaPrelevare) {
+		
+		float saldoArrivare = 0;
+		
+		saldoArrivare = this.saldo - sommaDaPrelevare;
+		
+		if(this.saldo > 0 && saldoArrivare >= 0) {
+			
+			this.saldo -= sommaDaPrelevare;
+			
+		}else {
+			
+			System.out.println("impossibile continuare saldo in negativo");
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "nome : " + getNomeProprietario() + " numero conto : " + getNumeroConto() + " saldo : " + getSaldo();
+	}
 }
